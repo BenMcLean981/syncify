32abd7f6d46075da3ebec6f81218a0e94a8f51c2
Ben McLean <35312653+BenMcLean981@users.noreply.github.com>
2025-02-08 15:29:45 -0500

    Brought in rough branch synchronization hook.

--- packages/react/src/spec/add.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from "vitest";
-import { add } from "../index.js";
-
-describe("add", () => {
-  it("Adds two numbers.", () => {
-    expect(add(1, 2)).toBe(3);
-  });
-});
